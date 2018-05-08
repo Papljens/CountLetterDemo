59a62bbc7f94372c00423829a25c0801e04306f1
PaplJens <38987333+Papljens@users.noreply.github.com>
2018-05-08 11:57:35 +0200

    -added CORS Filter (Working)

---
+++ org.demo.lettercount/src/main/java/org/demo/lettercount/rest/CorsFilter.java
@@ -0,0 +1,41 @@
+package org.demo.lettercount.rest;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+
+/**
+*Jersey REST CORS Filter
+*
+*Cross-Origin Resource Sharing
+*
+*Interface to create a custom filter which will inject Access-Control-Allow-* header to each outgoing request and enable the CORS mechanism
+**Mind the part in web.xml
+*
+**@author  PaplJens
+*
+*/
+
+
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+ 
+    @Override
+    public void filter(ContainerRequestContext requestContext, 
+      ContainerResponseContext responseContext) throws IOException {
+          responseContext.getHeaders().add(
+            "Access-Control-Allow-Origin", "*");
+          responseContext.getHeaders().add(
+            "Access-Control-Allow-Credentials", "true");
+          responseContext.getHeaders().add(
+           "Access-Control-Allow-Headers",
+           "origin, content-type, accept, authorization");
+          responseContext.getHeaders().add(
+            "Access-Control-Allow-Methods", 
+            "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    }
+}
